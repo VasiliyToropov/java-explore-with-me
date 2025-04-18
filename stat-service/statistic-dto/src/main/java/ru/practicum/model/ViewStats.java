283adb0ff97e93d03a8dfc52ac0156c2a2fcc54a
Vasiliy <toropovforwork@mail.ru>
2025-04-18 19:24:05 +0300

    Добавление основного сервиса, повторная прогонка тестов

--- stat-service/statistic-dto/src/main/java/ru/practicum/model/ViewStats.java
+++ stat-service/statistic-dto/src/main/java/ru/practicum/model/ViewStats.java
@@ -1,16 +1,19 @@
 package ru.practicum.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Getter
 @Setter
 public class ViewStats {
     private String app;
     private String uri;
     private Long hits;
+
+    public ViewStats(String app, String uri, Long hits) {
+        this.app = app;
+        this.uri = uri;
+        this.hits = hits;
+    }
 }
